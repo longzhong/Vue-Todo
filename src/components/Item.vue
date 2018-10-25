a04e54d7aa7a61536cb24a835125bedccb43ba2a
longzhong <59749706@qq.com>
2018-10-25 15:35:38 +0800

    102500

---
+++ src/components/Item.vue
@@ -0,0 +1,46 @@
+<template>
+    <div :class="['todo-item',todo.completed ? 'completed' : '']">
+        <input type="checkbox" class="toggle" v-model="todo.completed">
+        <label>{{todo.content}}</label>
+        <button class="destory" @click="deleteTodo"></button>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        todo: {
+            type: Object,
+            required: true
+        }
+    },
+    methods: {
+        deleteTodo() {
+            this.$emit('del', this.todo.id)
+        }
+    }
+}
+</script>
+
+<style lang=stylus scoped>
+.todo-item {
+    vertical-align: text-top;
+    border: none;
+    padding: 15px 2%;
+    width: 88%;
+    margin: 10px 4%;
+    border-bottom: 1px solid #eee;
+    font-size: 28px;
+
+    input[type='checkbox'], input[type='checkbox']:checked {
+        vertical-align: text-top;
+        width: 30px;
+        height: 30px;
+    }
+
+    .destory {
+        width: 30px;
+        height: 30px;
+        float: right;
+    }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Todo from '@/components/Todo'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  routes: [{
+    path: '/',
+    name: 'Todo',
+    component: Todo
+  }]
 })
